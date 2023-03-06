0dde45deca9519a692acf49107a2d9a6063b8e25
mhenriette <mhenriette00@gnmail.com>
2023-03-06 19:16:38 +0200

    refactor: format Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,33 +1,17 @@
 import React from "react"
+import NoteTitle from "./NoteTitle"
 
 export default function Sidebar(props) {
-    const NoteElements = () => {
-        return props.notes.map((note, index) => (
-            <div key={note.id}>
-                <div
-                    className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
-                        }`}
-                    onClick={() => props.setCurrentNoteId(note.id)}
-                >
-                    <h4 className="text-snippet">{note.body.split('\n')[0]}</h4>
-                    <button
-                        className="delete-btn"
-                        onClick={(e) => props.deleteNote(e, note.id)}
-                    >
-                        <i className="gg-trash trash-icon"></i>
-                    </button>
-                </div>
-            </div>
-        ))
-    }
-
     return (
         <section className="pane sidebar">
             <div className="sidebar--header">
                 <h3>Notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
             </div>
-            <NoteElements />
+            {props.notes.map((note) => (
+                <NoteTitle note={note} key={note.id} deleteNote={props.deleteNote} setCurrentNoteId={props.setCurrentNoteId} currentNote={props.currentNote} />
+            ))}
+
         </section>
     )
 }
